9fd2fa1db31574800daa040915d95ebed113e251
dferreto <diferretomo@est.utn.ac.cr>
2024-07-24 03:57:10 -0600

    cambios en el index, menu y about

--- public/sections/menu.js
+++ public/sections/menu.js
@@ -1,45 +1,103 @@
 class Menu extends HTMLElement{
     constructor(){
         super();
-        this.innerHTML=` <nav class="navbar navbar-expand-lg navbar-light fondo-menu fixed-top">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="index.html">
-                <img src="img/logo/logo-oficial.png" alt="" width="65%" class="d-inline-block align-text-top">
+        this.innerHTML=`
+<nav class="navbar navbar-expand-lg bg-body-tertiary fixed-top">
+  <div class="container">
+    <a class="navbar-brand fs-4" href="index.html">
+    <img src="img/logo/logo.png" alt="" width="70px" height="70px" class="d-inline-block align-text-top logo">
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+      <div class="offcanvas-header">
+        <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+      </div>
+      <div class="offcanvas-body">
+        <ul class="navbar-nav justify-content-center flex-grow-1 pe-3">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Investigaciones
             </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="fw-bold collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav me-auto">
-                    <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="index.html">Inicio</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link"  href="allInvestigation.html">Investigaciones</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="investigation.html">Subir investigaciones</a>
-                    </li>
-                </ul>
-                <ul class="nav-item dropdown active posicion-menu">
-									<a id="userName" class="nav-link dropdown-toggle invitado" href="#" role="button" data-bs-toggle="dropdown"
-									   aria-expanded="false">
-										Invitado
-									</a>
-									<ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink_01">
-										<li><a class="dropdown-item" href="login.html">Iniciar sesión</a></li>
-										<li><a class="dropdown-item" href="#" onclick="salir()">Cerrar sesión</a></li>
-									</ul>
-								</ul>
-                <ul class="navbar-nav">
-                <li class="nav-item text-center">
-    <img id="userPhoto" src="img/logo/perfil.png" alt="User Photo" 
-    style="width: 70px; height: 70px; border-radius: 50%; border: 2px solid black;">
-        </li>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#">Consultar investigaciones</a></li>
+            <li><a class="dropdown-item" href="#">Subir investigaciones</a></li>
             </ul>
-            </div>
-        </div>
-    </nav>`
+          </li>
+        </ul>
+        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+        <li class="nav-item dropdown me-3">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Invitado
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Iniciar sesión</a></li>
+            <li><a class="dropdown-item" href="#">Cerrar sesión</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <img id="userPhoto" src="img/logo/perfil.png" alt="User Photo" 
+               style="width: 50px; height: 50px; border-radius: 50%; border: 2px solid black;">
+        </li>
+      </ul>
+      </div>
+    </div>
+  </div>
+</nav>
+
+ <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="index.html">
+      <img src="img/logo/logo2.jpeg" alt="" width="70px" height="70px" class="d-inline-block align-text-top logo">
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Investigación
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Consultar investigaciones</a></li>
+            <li><a class="dropdown-item" href="#">Subir investigaciones</a></li>
+          </ul>
+        </li>
+      </ul>
+
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+        <li class="nav-item dropdown me-3">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Invitado
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Iniciar sesión</a></li>
+            <li><a class="dropdown-item" href="#">Cerrar sesión</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <img id="userPhoto" src="img/logo/perfil.png" alt="User Photo" 
+               style="width: 50px; height: 50px; border-radius: 50%; border: 2px solid black;">
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+ `
+
+ 
+
+    
     }
 
    
