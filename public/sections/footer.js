834e283b9d6fbf1dbca99c8851fcd44ceb12f364
dferreto <diferretomo@est.utn.ac.cr>
2024-07-24 18:56:12 -0600

    footer finalizado, y arreglos del about e index

--- public/sections/footer.js
+++ public/sections/footer.js
@@ -1,28 +1,50 @@
-class Footer extends HTMLElement{
-    constructor(){
+class Footer extends HTMLElement {
+    constructor() {
         super();
-        this.innerHTML=`<div class="">
-        <br><hr>
-<div class="div_pi">
-    <strong>Ubicados: </strong>Costa Rica, Puntarenas.<br/>
-    <strong>eMail:    </strong> <a href="mailto:mcgyverweb@gmail.com?subject=Solicito información">mcgyverweb@gmail.com</a><br />
-    <strong>Teléfono: </strong>7181-7325
-</div>
-<div class="div_pc">
-    Carrera de Tecnologías de la información<br/>
-    Curso de Tecnologías y Sistemas Web 3<br/>
-    Desarrollado completamente a mano de expertos
-</div>
-<div class="redes-containar">
-    <ul>
-    <li><a href="https://www.facebook.com/" class="facebook"><i class="fa fa-facebook"></i></a></li>
-    <li><a href="https://x.com/" class="twitter"><i class="fa-brands fa-x-twitter"></i></a></li>
-    <li><a href="https://instagram.com/" class="instagram"><i class="fa fa-instagram"></i></a></li>
-    </ul>
-</div>
-</div>` 
-    }     
+        this.innerHTML = `
+<footer class="bg-dark text-light py-4 mt-auto fondo-footer pie">
+  <div class="container">
+    <div class="row text-center text-md-left">
+      <div class="col-md-4 mb-3 div_pi">
+        <h5 class="text-left">Contactos</h5>
+        <p class="text-left">
+          <strong>Costa Rica, Puntarenas.</strong><br/>
+          <strong>Email:</strong> <a href="mailto:ferretomoraga22@outlook.com?subject=Solicito información" class="text-light">ferretomoraga22@outlook.com</a><br />
+          <strong>Teléfono:</strong> 7181-7325
+        </p>
+      </div>
+      <div class="col-md-4 mb-3 div_pc">
+        <h5>Información</h5>
+        <p>
+       
+          Desarrollado completamente a mano por expertos en<br/>
+             Tecnologías de la Información
+        </p>
+      </div>
+      <div class="col-md-4 mb-3 text-center redes-containar">
+        <h5>Redes Sociales</h5>
+        <ul class="list-unstyled d-flex justify-content-center">
+          <li class="me-3">
+            <a href="https://www.facebook.com/dinier.ferreto" class="facebook" aria-label="Facebook"><i class="fa fa-facebook"></i></a>
+          </li>
+          <li class="me-3">
+            <a href="https://www.linkedin.com/in/dinier-ferreto-5a979027a/" class="linkedin" aria-label="LinkedIn"><i class="fa-brands fa-linkedin"></i></a>
+          </li>
+          <li>
+            <a href="https://www.instagram.com/dinno_30/" class="instagram" aria-label="Instagram"><i class="fa fa-instagram"></i></a>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <hr class="my-4">
+    <div class="text-center">
+      <p class="mb-0">&copy; 2024 LaToñita. Todos los derechos reservados.</p>
+    </div>
+  </div>
+</footer>
+
+`;
+    }
 }
 
 customElements.define('footer-component', Footer);
-
